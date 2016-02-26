7c94e55008a9f82ab4ec7bc72df55e790325f2f9
Brendan Callahan <brendan.callahan@gmail.com>
2016-02-25 22:46:52 -0500

    moving commented out s3 stuff into sandbox folder

--- scrape-images.py
+++ scrape-images.py
@@ -6,7 +6,6 @@ import shutil
 import json
 import optparse
 from collections import defaultdict
-import boto3
 import socket
 import traceback
 
@@ -265,16 +264,3 @@ class GoogleImageScraper(object):
 # initialize the image scraper class with the comand line options, then process all the words
 image_scraper = GoogleImageScraper(opts)
 image_scraper.process_all_words()
-
-
-#  when S3 mode is turned on, upload the file over to S3 and delete the local copy
-# TODO can we simplify this so it doesn't need to write out to a tmp location?
-# TODO add metadata like original link to storage?
-# S3_BUCKET_NAME = 'brendan.callahan'
-# S3_BASE_PATH = 'thesis/images/'+opts.language+'/'
-# if STORAGE_MODE == 'S3':
-#     word_index_str = "{0:0=2d}".format(word_index)
-#     destination_path = S3_BASE_PATH+word_index_str+'/'+actual_file_name
-#     s3_client = boto3.resource('s3')
-#     s3_client.meta.client.upload_file(full_path, S3_BUCKET_NAME, destination_path)
-#     os.remove(full_path)
