3f876732546f8790485a1b56474b36af4ed0aed0
Brendan Callahan <brendan.callahan@gmail.com>
2016-03-01 00:24:58 -0500

    break out two more classes, one for for processing all of the images for each word, and one for processing each image downloading thread. add multi-threaded support for processing each image returned in the search result and downloading it. default number of threads to 4 for now for a decent boost. ensure thread safety throughout

--- scrape-images.py
+++ scrape-images.py
@@ -8,6 +8,7 @@ import optparse
 import random
 from collections import defaultdict
 import traceback
+import threading
 
 from selenium import webdriver
 
@@ -43,7 +44,191 @@ GOOGLE_METADATA_XPATH = "//div[@class='rg_meta']"
 # Note: check for presence in list is also lowercased
 VALID_FILE_EXTENSIONS = ['jpg', 'jpeg', 'gif', 'png', 'ico', 'bmp', 'svg']
 
+# 4x gives us a decent boost, without being too "spammy", hopefully
+threadLimiter = threading.BoundedSemaphore(4)
+
+# lock for printing to console
+printing_lock = threading.Lock()
+
 DEBUG_MODE = False
+
+
+# defines the downloader threads so that we can download images in a concurrent way
+class DownloadThread(threading.Thread):
+    def __init__(self, word_downloader, href_attribute, link_index, current_metadata, base_path_for_word, user_agent):
+        self.word_downloader = word_downloader
+        self.href_attribute = href_attribute
+        self.link_index = link_index
+        self.current_metadata = current_metadata
+        self.base_path_for_word = base_path_for_word
+        self.user_agent = user_agent
+        threading.Thread.__init__(self)
+
+    # simple wrapper function for thread safe console printing,
+    # so we can wrap all of our multi-threaded console output in one function
+    def thread_safe_print(self, output):
+        with printing_lock:
+            print(output)
+
+    # takes the link element from xpath, dissects out the href attribute, and runs a regex to extract the source URL
+    def get_image_link(self, href_attribute):
+        # some links are double or triple quoted, by chaining this call 3 times, we seem to unquote most of it
+        # there is no functional downside that i know of for unquoting more than need be
+        triple_decoded_attr = urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(href_attribute)))
+        regex_result = re.search(IMAGE_URL_REGEX, triple_decoded_attr)
+        return regex_result.group('url')
+
+    def run(self):
+        threadLimiter.acquire()
+        try:
+            # take the list href attribute, and extract the actual image link from that compound string
+            actual_image_link = self.get_image_link(self.href_attribute)
+
+            # force the link index string to be two digits (like 01, 02, etc), and then store the
+            link_index_str = "{0:0=2d}".format(self.link_index+1)
+
+            # take the metadata stored in an adjacent div as JSON, load it as a hash, and add it to our metadata
+            google_metadata_for_image = json.loads(self.current_metadata)
+
+            metadata_for_image = {'image_link': actual_image_link, 'google': google_metadata_for_image}
+
+            # extract the actual file name and the file extension
+            actual_file_name = actual_image_link.split('/')[-1]
+            metadata_for_image['original_filename'] = actual_file_name
+            file_extension = actual_file_name.split('.')[-1]
+            no_extension = False
+
+            # ggpht images seem to be internal to the search engine results, skipping them
+            if actual_image_link.find('ggpht.com/') != -1:
+                self.thread_safe_print('Skipped ggpht link: ' + actual_image_link)
+                metadata_for_image['skipped'] = True
+            else:
+
+                # if the file path does not have a valid extension, mark this as such so that we can set it later
+                # based on the actual content type (which typically falls in our allowed list)
+                if not file_extension.lower() in VALID_FILE_EXTENSIONS:
+                    no_extension = True
+
+                # when in debug mode, just print the link out, otherwise download the file
+                if DEBUG_MODE:
+                    self.thread_safe_print(actual_image_link)
+                else:
+                    # make sure we re-quote the link for the sake of any special characters.
+                    # Note: we need to skip the :/ for the http:// and the uri component separators, the ?&= for
+                    # query arguments, and the comma also blows up on us
+                    quoted_image_link = urllib.parse.quote(actual_image_link, ':/,?&=')
+
+                    if opts.verbose_mode: self.thread_safe_print('Downloading... ' + quoted_image_link)
+
+                    metadata_for_image['filename'] = link_index_str+'.'+file_extension
+
+                    # get the full path where we will store the image, e.g. base_path/01.jpg
+                    full_path = self.base_path_for_word+metadata_for_image['filename']
+
+                    try:
+                        # Crucial, fudge the user-agent string here so that network admins don't think we are
+                        # urllib, since anything "programmatic" is automatically conflated with an "attack"
+                        # quote for the sake of special characters
+                        request = urllib.request.Request(quoted_image_link, None, {
+                            'User-Agent': self.user_agent
+                        })
+                        with urllib.request.urlopen(request, timeout=30) as response, open(full_path, 'wb') as out_file:
+                            content_type = response.info().get_content_type()
+                            shutil.copyfileobj(response, out_file)
+
+                        # if there was no file extension and this is a content-type of image,
+                        # lets use the content-type from the request to set it, [6:] takes everything after image/
+                        # note: with the 1-liner above for the file path, it seems we need this workaround
+                        # TODO: should probably deconstruct it
+                        if no_extension and content_type.startswith('image/'):
+                            original_file_path = full_path
+                            new_file_path = link_index_str + '.' + content_type[6:]
+                            shutil.move(original_file_path, new_file_path)
+
+                        metadata_for_image['success'] = True
+
+                    # catch some 503/504 type errors and also if the link points to a directory rather than a file
+                    # typically errors like:
+                    # (urllib.error.HTTPError, IsADirectoryError, socket.timeout, urllib.error.URLError, ConnectionResetError)
+                    # extended to be indiscriminate since from the perspective of the full scrape, we probably don't care whether
+                    # we know about the error yet
+                    # TODO log error counts?
+                    except Exception as e:
+                        error_class = type(e).__name__
+                        self.word_downloader.increment_error_count_for_class(error_class)
+                        metadata_for_image['success'] = False
+                        metadata_for_image['error_class'] = error_class
+                        self.thread_safe_print('Failed to fetch:' + quoted_image_link + ' due to: ' + error_class +
+                                               ' unquoted:' + actual_image_link)
+                        if DEBUG_MODE: self.thread_safe_print(traceback.format_exc())
+
+                self.word_downloader.add_metadata_for_word_index(link_index_str, metadata_for_image)
+
+        finally:
+            threadLimiter.release()
+
+class WordImageDownloader:
+    def __init__(self, scraper, word, word_index, href_attributes, metadatas):
+        self.scraper = scraper
+        self.word = word
+        self.word_index = word_index
+        self.href_attributes = href_attributes
+        self.metadatas = metadatas
+
+        # track a dictionary with the errors for the current words
+        self.current_word_download_errors = defaultdict(int)
+
+        self.base_path_for_word = opts.base_image_path+str(self.word_index)+'/'
+        # ensure directory is created for this word index
+        if not os.path.exists(self.base_path_for_word): os.makedirs(self.base_path_for_word)
+
+        # hash to store the metadata information for each image we download, keyed by the word index
+        self.image_metadata_for_word = {}
+
+        # lock to ensure thread safe incrementing of error classes within error class count tracking hashes
+        self.error_hash_lock = threading.Lock()
+
+        # lock to ensure thread safe updating of the metadata hash for all the images for this word
+        self.image_metadata_lock = threading.Lock()
+
+    # function that updates the count of this error for the word, and for all words
+    def increment_error_count_for_class(self, error_class):
+        with self.error_hash_lock:
+            self.current_word_download_errors[error_class] += 1
+            self.scraper.increment_error_count_for_class(error_class)
+
+    # function that updates the metadata, given a link index and metadata payload.
+    # extra cautious with a lock in case anything might go wrong thread-safety wise
+    def add_metadata_for_word_index(self, link_index_str, metadata):
+        with self.image_metadata_lock:
+            self.image_metadata_for_word[link_index_str] = metadata
+
+    # main function that drives processing the current word, creates a bunch of download threads and then fires
+    # them off
+    def process_word(self):
+        thread_list = []
+        for link_index, href_attribute in enumerate(self.href_attributes):
+            current_metadata = self.metadatas[link_index]
+            user_agent = self.scraper.get_random_user_agent()
+            current_thread = DownloadThread(self, href_attribute, link_index, current_metadata, self.base_path_for_word, user_agent)
+            thread_list.append(current_thread)
+
+        for t in thread_list:
+            t.start()
+
+        for t in thread_list:
+            t.join()
+
+        json.dump(self.image_metadata_for_word, open(self.base_path_for_word+'metadata.json', 'w'))
+        json.dump(self.current_word_download_errors, open(self.base_path_for_word+'errors.json', 'w'))
+
+        # exit after first word in debug mode
+        if DEBUG_MODE:
+            # sleep 10 seconds between google searches in debug mode to prevent hammering
+            time.sleep(10)
+
+            # arbitrary testing point of 100 iterations before exiting in debug mode
+            if self.word_index > 100: exit()
 
 # class that handles image scraping in google
 class GoogleImageScraper(object):
@@ -76,19 +261,18 @@ class GoogleImageScraper(object):
         if len(current_language_entry['lr']) > 0:
             self.base_language_search_url += '&lr=' + current_language_entry['lr']
 
+    # accessor method for incrementing the count of errors for a class
+    def increment_error_count_for_class(self, error_class):
+        self.all_word_download_errors[error_class] += 1
+
+    # simple function to grab a random user agent
+    def get_random_user_agent(self):
+        return random.choice(self.user_agent_list)
 
     # creates a selenium browser instance with Firefox
     def create_selenium_browser(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(10)#10 is arbitrary
-
-    # takes the link element from xpath, dissects out the href attribute, and runs a regex to extract the source URL
-    def get_image_link(self, href_attribute):
-        # some links are double or triple quoted, by chaining this call 3 times, we seem to unquote most of it
-        # there is no functional downside that i know of for unquoting more than need be
-        triple_decoded_attr = urllib.parse.unquote(urllib.parse.unquote(urllib.parse.unquote(href_attribute)))
-        regex_result = re.search(IMAGE_URL_REGEX, triple_decoded_attr)
-        return regex_result.group('url')
 
     # get the href attributes that contain the underlying image links
     def get_href_attributes_for_word(self, word):
@@ -146,114 +330,9 @@ class GoogleImageScraper(object):
                 print('Current word: ' + foreign_word + ' at index: ' + str(word_index))
                 print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
-            # track a dictionary with the errors for the current words
-            current_word_download_errors = defaultdict(int)
-
-            base_path_for_index = opts.base_image_path+str(word_index)+'/'
-            # ensure directory is created for this word index
-            if not os.path.exists(base_path_for_index): os.makedirs(base_path_for_index)
-
-            # array to store the metadata dictionaries for each image we download
-            list_of_image_metadata = []
-
             href_attributes, metadatas = self.get_href_attributes_for_word(foreign_word)
-
-            for link_index, href_attribute in enumerate(href_attributes):
-                # take the list href attribute, and extract the actual image link from that compound string
-                actual_image_link = self.get_image_link(href_attribute)
-
-                # force the link index string to be two digits (like 01, 02, etc), and then store the
-                link_index_str = "{0:0=2d}".format(link_index+1)
-
-                # take the metadata stored in an adjacent div as JSON, load it as a hash, and add it to our metadata
-                google_metadata_for_image = json.loads(metadatas[link_index])
-
-                metadata_for_image = {'image_link': actual_image_link, 'google': google_metadata_for_image}
-
-                # extract the actual file name and the file extension
-                actual_file_name = actual_image_link.split('/')[-1]
-                metadata_for_image['original_filename'] = actual_file_name
-                file_extension = actual_file_name.split('.')[-1]
-                no_extension = False
-
-                # ggpht images seem to be internal to the search engine results, skipping them
-                if actual_image_link.find('ggpht.com/') != -1:
-                    print('Skipped ggpht link: ' + actual_image_link)
-                    metadata_for_image['skipped'] = True
-                    list_of_image_metadata.append(metadata_for_image)
-                    continue
-
-                # if the file path does not have a valid extension, mark this as such so that we can set it later
-                # based on the actual content type (which typically falls in our allowed list)
-                if not file_extension.lower() in VALID_FILE_EXTENSIONS:
-                    no_extension = True
-
-                # when in debug mode, just print the link out, otherwise download the file
-                if DEBUG_MODE:
-                    print(actual_image_link)
-                else:
-                    # make sure we re-quote the link for the sake of any special characters.
-                    # Note: we need to skip the :/ for the http:// and the uri component separators, the ?&= for
-                    # query arguments, and the comma also blows up on us
-                    quoted_image_link = urllib.parse.quote(actual_image_link, ':/,?&=')
-
-                    if opts.verbose_mode: print('Downloading... ' + quoted_image_link)
-
-                    metadata_for_image['filename'] = link_index_str+'.'+file_extension
-
-                    # get the full path where we will store the image, e.g. base_path/01.jpg
-                    full_path = base_path_for_index+metadata_for_image['filename']
-
-                    try:
-                        # Crucial, fudge the user-agent string here so that network admins don't think we are
-                        # urllib, since anything "programmatic" is automatically conflated with an "attack"
-                        # quote for the sake of special characters
-                        request = urllib.request.Request(quoted_image_link, None, {
-                            'User-Agent': random.choice(self.user_agent_list)
-                        })
-                        with urllib.request.urlopen(request, timeout=30) as response, open(full_path, 'wb') as out_file:
-                            content_type = response.info().get_content_type()
-                            shutil.copyfileobj(response, out_file)
-
-                        # if there was no file extension and this is a content-type of image,
-                        # lets use the content-type from the request to set it, [6:] takes everything after image/
-                        # note: with the 1-liner above for the file path, it seems we need this workaround
-                        # TODO: should probably deconstruct it
-                        if no_extension and content_type.startswith('image/'):
-                            original_file_path = full_path
-                            new_file_path = link_index_str + '.' + content_type[6:]
-                            shutil.move(original_file_path, new_file_path)
-
-                        metadata_for_image['success'] = True
-
-                    # catch some 503/504 type errors and also if the link points to a directory rather than a file
-                    # typically errors like:
-                    # (urllib.error.HTTPError, IsADirectoryError, socket.timeout, urllib.error.URLError, ConnectionResetError)
-                    # extended to be indiscriminate since from the perspective of the full scrape, we probably don't care whether
-                    # we know about the error yet
-                    # TODO log error counts?
-                    except Exception as e:
-                        error_class = type(e).__name__
-                        self.all_word_download_errors[error_class] += 1
-                        current_word_download_errors[error_class] += 1
-                        metadata_for_image['success'] = False
-                        metadata_for_image['error_class'] = error_class
-                        print('Failed to fetch:' + quoted_image_link + ' due to: ' + error_class + ' unquoted:' +
-                              actual_image_link)
-                        if DEBUG_MODE: print(traceback.format_exc())
-
-                list_of_image_metadata.append(metadata_for_image)
-
-            json.dump(list_of_image_metadata, open(base_path_for_index+'metadata.json', 'w'))
-            json.dump(current_word_download_errors, open(base_path_for_index+'errors.json', 'w'))
-
-            # exit after first word in debug mode
-            if DEBUG_MODE:
-                # sleep 10 seconds between google searches in debug mode to prevent hammering
-                time.sleep(10)
-
-                # arbitrary testing point of 100 iterations before exiting in debug mode
-                if word_index > 100: exit()
+            word_image_downloader = WordImageDownloader(self, foreign_word, word_index, href_attributes, metadatas)
+            word_image_downloader.process_word()
 
         json.dump(self.all_word_download_errors, open(opts.base_image_path+'all_errors.json', 'w'))
 
