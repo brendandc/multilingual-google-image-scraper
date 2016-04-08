d61da18144a6e378c5a68ed52a88e759cd189639
Brendan Callahan <brendan.callahan@gmail.com>
2016-04-07 22:35:47 -0400

    adding first pass of script to extract the language tar into original form

---
+++ extract_language_package.py
@@ -0,0 +1,27 @@
+import optparse
+import os
+import glob
+
+optparser = optparse.OptionParser()
+optparser.add_option("-f", "--filename", dest="filename", help="Language package file")
+optparser.add_option("-d", "--destination", dest="destination", help="Base destination folder")
+optparser.add_option("-l", "--language", dest="language", help="Language to un-package")
+(opts, _) = optparser.parse_args()
+
+full_destination_path = opts.destination + "/" + opts.language
+
+if not os.path.exists(full_destination_path):
+    os.makedirs(full_destination_path)
+
+outer_untar_command = "tar -xvf " + opts.filename + " -C " + full_destination_path
+print(outer_untar_command)
+os.system(outer_untar_command)
+
+for inner_filename in glob.glob(full_destination_path+"/*.tar.gz"):
+    inner_untar_command = "tar -xvzf " + inner_filename + " -C " + full_destination_path
+    print(inner_untar_command)
+    os.system(inner_untar_command)
+
+delete_intermediate_files_command = "rm " + full_destination_path + "/*.tar.gz"
+print(delete_intermediate_files_command)
+os.system(delete_intermediate_files_command)
