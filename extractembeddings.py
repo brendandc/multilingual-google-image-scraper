e528115812e19c4c1cdcda0b3ad45cd74fb3375f
Sarah Payne <paynesa@tesla.ldc.upenn.edu>
2019-11-13 20:34:13 -0500

    averaging embeddings

--- extractembeddings.py
+++ extractembeddings.py
@@ -5,7 +5,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from torch.autograd import Variable 
 from PIL import Image
-model = models.resnet18(pretrained=True)
+model = models.resnet34(pretrained=True)
 layer = model._modules.get('avgpool')
 model.eval()
 scaler = transforms.Scale((224, 224))
@@ -26,14 +26,20 @@ def get_vector(image_name):
 model = models.resnet18(pretrained=True)
 layer = model._modules.get('avgpool')
 model.eval()
-scaler = transforms.Scale((224, 224))
+scaler = transforms.Resize((224, 224))
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 to_tensor = transforms.ToTensor()
+l = []
 for file in os.listdir('0'):
 	x = get_vector(file).data.numpy()[0, :, 0, 0]
+	l.append(x)
 	with open('testembeddings.txt', 'a') as f:
 		numpy.savetxt(f, x.reshape(1, 512), fmt="%s")
 	print(file)
+average = numpy.zeros(512,)
+for embedding in l:
+	average+= embedding
+print(embedding / len(l))
 
 
 		
